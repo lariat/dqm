022d0518d81b4ea5d69a3b37bbd0648b94be6262
Johnny Ho <johnnyho@uchicago.edu>
2015-03-07 16:46:42 -0600

    Needed a function that was commented out.

--- rawdatautils/rawdatautils.py
+++ rawdatautils/rawdatautils.py
@@ -92,32 +92,32 @@ def get_wut_time_header(file_path):
 #        ])
 #
 #    return waveform_array
-#
-#def get_mwpc_tdc_hit_time(file_path):
-#
-#    mwpc_branch_list = [
-#        'tdc_number',
-#        'hit_channel',
-#        'hit_time_bin',
-#        ]
-#
-#    mwpc_array = rnp.root2array(file_path, 'DataQuality/mwpc',
-#                                mwpc_branch_list)
-#
-#    number_tdcs = 16
-#    number_triggers = mwpc_array.size
-#    tdc_number = mwpc_array['tdc_number']
-#    hit_channel = mwpc_array['hit_channel']
-#    hit_time_bin = mwpc_array['hit_time_bin']
-#
-#    hit_time = [ [] for i in range(number_tdcs) ]
-#
-#    for trigger in xrange(number_triggers):
-#        for tdc_index in xrange(number_tdcs):
-#            flag = (tdc_number[trigger] == tdc_index + 1)
-#            hit_time[tdc_index].extend(hit_time_bin[trigger][flag])
-#
-#    return np.array(hit_time)
-#
+
+def get_mwpc_tdc_hit_time(file_path):
+
+    mwpc_branch_list = [
+        'tdc_number',
+        'hit_channel',
+        'hit_time_bin',
+        ]
+
+    mwpc_array = rnp.root2array(file_path, 'DataQuality/mwpc',
+                                mwpc_branch_list)
+
+    number_tdcs = 16
+    number_triggers = mwpc_array.size
+    tdc_number = mwpc_array['tdc_number']
+    hit_channel = mwpc_array['hit_channel']
+    hit_time_bin = mwpc_array['hit_time_bin']
+
+    hit_time = [ [] for i in range(number_tdcs) ]
+
+    for trigger in xrange(number_triggers):
+        for tdc_index in xrange(number_tdcs):
+            flag = (tdc_number[trigger] == tdc_index + 1)
+            hit_time[tdc_index].extend(hit_time_bin[trigger][flag])
+
+    return np.array(hit_time)
+
 #def get_wut_hits(file_path):
 #    return
