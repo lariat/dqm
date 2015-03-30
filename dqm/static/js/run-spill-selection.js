3f4d7b1afea0e668900c6ae7145b4e71a8fd174a
Johnny Ho <johnnyho@uchicago.edu>
2015-03-29 22:12:07 -0500

    Completed spills now populate the spills dropdown menu as they are processed.

--- dqm/static/js/run-spill-selection.js
+++ dqm/static/js/run-spill-selection.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  var number_completed_spills = 0;
+
   $.getJSON($SCRIPT_ROOT + '/json?q=runs', function(data) {
     $('#selected-run').html(data.selected);
     $("#run-selection").val(data.selected);
@@ -8,9 +10,30 @@ $(document).ready(function() {
     $('#runs').append('<li class="divider"></li>');
     $('#runs').append('<li class="dropdown-header">Completed Runs</li>');
     for (var i = 0; i < data.completed.length; ++i) {
-        $('#runs').append('<li class="run-option" value="' + data.completed[i] + '"><a href="#">' + data.completed[i] + '</a></li>');
+      $('#runs').append('<li class="run-option" value="' + data.completed[i] + '"><a href="#">' + data.completed[i] + '</a></li>');
     }
 
+    ajax_runs();
+
+  });
+
+  $.getJSON($SCRIPT_ROOT + '/json?q=spills', function(data) {
+
+    number_completed_spills = data.completed.length;
+
+    $('#selected-spill').html(data.selected);
+    $('#spills').append('<li class="spill-option" value="All"><a href="#">All</a></li>');
+    $('#spills').append('<li class="divider"></li>');
+    $('#spills').append('<li class="dropdown-header">Completed Spills</li>');
+    for (var i = 0; i < data.completed.length; ++i) {
+      $('#spills').append('<li class="spill-option" value="' + data.completed[i] + '"><a href="#">' + data.completed[i] + '</a></li>');
+    }
+
+    ajax_spills();
+
+  });
+
+  function ajax_runs() {
     $('#runs .run-option').click(function(e) {
       e.preventDefault();
       $("#run-selection").val($(this).attr("value"));
@@ -25,18 +48,9 @@ $(document).ready(function() {
         }
       });
     });
+  }
 
-  });
-
-  $.getJSON($SCRIPT_ROOT + '/json?q=spills', function(data) {
-    $('#selected-spill').html(data.selected);
-    $('#spills').append('<li class="spill-option" value="All"><a href="#">All</a></li>');
-    $('#spills').append('<li class="divider"></li>');
-    $('#spills').append('<li class="dropdown-header">Completed Spills</li>');
-    for (var i = 0; i < data.completed.length; ++i) {
-        $('#spills').append('<li class="spill-option" value="' + data.completed[i] + '"><a href="#">' + data.completed[i] + '</a></li>');
-    }
-
+  function ajax_spills() {
     $('#spills .spill-option').click(function(e) {
       e.preventDefault();
       $("#spill-selection").val($(this).attr("value"));
@@ -51,7 +65,25 @@ $(document).ready(function() {
         }
       });
     });
+  }
 
-  });
+  function update_spills() {
+    $.getJSON($SCRIPT_ROOT + '/json?q=spills', function(data) {
+      if (number_completed_spills < data.completed.length) {
+        for (var i = number_completed_spills; i < data.completed.length; ++i) {
+          $('#spills').append('<li class="spill-option" value="' + data.completed[i] + '"><a href="#">' + data.completed[i] + '</a></li>');
+          number_completed_spills = data.completed.length;
+        }
+        ajax_spills();
+      }
+    });
+  }
+
+  var timeout_spills;
+  function load_next_spills() {
+    update_spills();
+    timeout_spills = setTimeout(load_next_spills, 15000);
+  }
+  load_next_spills();
 
 });
