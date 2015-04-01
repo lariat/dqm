fffdaada5affcc99421b878812f258429db783cd
Johnny Ho <johnnyho@uchicago.edu>
2015-04-01 15:47:50 -0500

    Testing notifications.

---
+++ dqm/static/js/toastr.options.js
@@ -0,0 +1,18 @@
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": true,
+  "progressBar": true,
+  "positionClass": "toast-bottom-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": 0,
+  "extendedTimeOut": 0,
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut",
+  "tapToDismiss": true
+}
