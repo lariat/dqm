67010d0d28bb7d3838bacc5e858a220790f3f124
Johnny Ho <johnnyho@uchicago.edu>
2015-04-10 15:30:44 -0500

    Added a clean-up script.

---
+++ watchdog/cleanup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+
+def remove(path):
+    """
+    Remove the file
+    """
+    try:
+        if os.path.exists(path):
+            os.remove(path)
+            sys.stdout.write("Removing file: {}".format(path) + '\n')
+            sys.stdout.flush()
+    except OSError:
+        sys.stdout.write("Unable to remove file: {}".format(path) + '\n')
+        sys.stdout.flush()
+
+def cleanup(days, path):
+    """
+    Removes files from the passed-in path that are older than
+    or equal to the number of days
+    """
+    time_in_secs = time.time() - (days * 24 * 60 * 60)
+    for root, dirs, files in os.walk(path, topdown=False):
+        for file_ in files:
+            full_path = os.path.join(root, file_)
+            stat = os.stat(full_path)
+
+            if stat.st_mtime <= time_in_secs:
+                remove(full_path)
+
+        print os.listdir(root)
+
+if __name__ == '__main__':
+    #days, path = int(sys.argv[1]), sys.argv[2]
+    days, path = float(sys.argv[1]), sys.argv[2]
+    cleanup(days, path)
